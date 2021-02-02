ae724e9c4f8f9ae890a8807a75700736f6105fb5
Moonki Choi <52124204+iknoom@users.noreply.github.com>
2021-02-03 07:47:01 +0900

    게시글 내용 앞 공백 수정 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,8 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl iknoom-springboot2-webservice | grep jar | awk '{print $1}')
+# CURRENT_PID=$(pgrep -fl iknoom-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl java | awk '{print $1}')
 
 echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
